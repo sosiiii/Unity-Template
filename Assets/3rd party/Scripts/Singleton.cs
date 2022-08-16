eda788d255bd24e0181bdeb48058da1a82bf8e2e
Adam Woska <ademwoska@gmail.com>
2022-08-16 22:15:45 +0200

    Singleton added

---
+++ Assets/3rd party/Scripts/Singleton.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public abstract class Singleton<T> : MonoBehaviour {
+    
+    private static Dictionary<Type, object> _singletons
+        = new Dictionary<Type, object>();
+
+    public static T Instance {
+        get {
+            return (T)_singletons[typeof(T)];
+        }
+    }
+
+    void OnEnable() {
+        if (_singletons.ContainsKey(GetType())) {
+            Destroy(this);
+        } else {
+            _singletons.Add(GetType(), this);
+            DontDestroyOnLoad(this);
+        }
+    }
+}
